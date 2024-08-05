6e1b56b57ecdff1e3412460d43dbece59000f492
shrineOT <logandarby912@outlook.com>
2024-08-05 02:25:07 -0400

    Add files via upload

---
+++ Includes.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef COMBINED_INCLUDES_H
+#define COMBINED_INCLUDES_H
+
+#include <d3d11.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#include <Shlobj.h>
+#include <sstream>
+#include <tchar.h>
+#include <iomanip>
+#include <tlhelp32.h>
+#include <filesystem>
+#include <intrin.h>
+#include <vector>
+#include "Helpers/imgui/imgui.h"
+#include "Helpers/imgui/imgui_impl_win32.h"
+#include "Helpers/imgui/imgui_impl_dx11.h"
+#include "Helpers/imgui/imgui_internal.h"
+#include "Helpers/Logger/Logger.h"
+#include "Helpers/random/xorstr.hpp"
+#include "Helpers/Hooking/MemHelper.h"
+#include "Helpers/SDK/SDK.h"
+#include "Cheat.h"
+
+#endif
